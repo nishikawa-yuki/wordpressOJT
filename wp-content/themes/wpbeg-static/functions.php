9349eecb18acf81fa0093ac90b283bf2244c7ea2
西川勇気 <sussy168168@gmail.com>
2021-06-18 17:10:21 +0900

    OJT終了

--- wp-content/themes/wpbeg-static/functions.php
+++ wp-content/themes/wpbeg-static/functions.php
@@ -4,16 +4,16 @@
     add_theme_support( 'title-tag' );
 
     //タイトル出力
-    function wpbeg_title( $title ) {
-        if ( is_front_page() && is_home() ) { //トップページなら
-            $title = get_bloginfo( 'name', 'display' );
-        } elseif ( is_singular() ) { //シングルページなら
-            $title = single_post_title( '', false );
-        }
-            return $title;
-        }
-    add_filter( 'pre_get_document_title', 'wpbeg_title' );
-    add_theme_support( 'post-thumbnails' ); 
+    // function wpbeg_title( $title ) {
+    //     if ( is_front_page() && is_home() ) { //トップページなら
+    //         $title = get_bloginfo( 'name', 'display' );
+    //     } elseif ( is_singular() ) { //シングルページなら
+    //         $title = single_post_title( '', false );
+    //     }
+    //         return $title;
+    //     }
+    // add_filter( 'pre_get_document_title', 'wpbeg_title' );
+    // add_theme_support( 'post-thumbnails' ); 
     function wpbeg_script() {
         wp_enqueue_style( 'Sacramento', '//fonts.googleapis.com/css?family=Sacramento&amp;amp;subset=latin-ext', array() );
         wp_enqueue_style( 'wpbeg', get_template_directory_uri() . '/css/wpbeg.css', array(), '1.0.0' );
--- wp-content/themes/wpbeg-static/index.php
+++ wp-content/themes/wpbeg-static/index.php
@@ -3,7 +3,7 @@
     <div class="main-view">
       <img class="main-view-img switch" src="<?php echo get_template_directory_uri(); ?>/images/visual_1_sp.png"alt="main-viewsp"> 
       <div class="logo2">
-      <img class="logo2 switch" src="<?php echo get_template_directory_uri(); ?>/images/visual_text_sp.png" alt="logo2">
+        <img class="logo2 switch" src="<?php echo get_template_directory_uri(); ?>/images/visual_text_sp.png" alt="logo2">
       </div>
     </div>
     <div class="main-view-text">
@@ -25,7 +25,7 @@
         </div>
       </div>
       <div class="up-box">
-        <img src="" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/post_img_2.png" alt="">
         <div class="text-box">
           <p class="up-date">2017.7.8</p>
           <p class="p-title">あのネオンはいつ交換するのか！？観覧車の謎に迫る</p>
@@ -35,7 +35,39 @@
         </div>
       </div>
       <div class="up-box">
-        <img src="" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/post_img_3.png" alt="">
+        <div class="text-box">
+          <p class="up-date">2017.7.8</p>
+          <p class="p-title">ラソナの社内はこんなよ</p>
+        </div>
+        <div class="readmore">
+          <a href="/" class="border">READ MORE</a>
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <div class="up-box">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/post_img_4.png" alt="">
+        <div class="text-box">
+          <p class="up-date">2018.5.12</p>
+          <p class="p-title">おしゃれなカフェあります</p>
+        </div>
+        <div class="readmore">
+          <a href="/" class="border">READ MORE</a>
+        </div>
+      </div>
+      <div class="up-box">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/post_img_5.png" alt="">
+        <div class="text-box">
+          <p class="up-date">2017.7.8</p>
+          <p class="p-title">あのネオンはいつ交換するのか！？観覧車の謎に迫る</p>
+        </div>
+        <div class="readmore">
+          <a href="/" class="border">READ MORE</a>
+        </div>
+      </div>
+      <div class="up-box">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/post_img_6.png" alt="">
         <div class="text-box">
           <p class="up-date">2017.7.8</p>
           <p class="p-title">ラソナの社内はこんなよ</p>
